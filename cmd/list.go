83ba557814891dc79767ce2263957467c6db2376
TheBoringDude <iamcoderx@gmail.com>
2022-08-02 10:55:31 +0800

    docs: change help info of `list` command

--- cmd/list.go
+++ cmd/list.go
@@ -18,13 +18,8 @@ import (
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "List your created wax wallets",
+	Long:  `List the wallets that you have created with their keys`,
 	Run: func(cmd *cobra.Command, args []string) {
 		list := db.List()
 		if len(list) == 0 {
